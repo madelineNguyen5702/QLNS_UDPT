9d6b511d481a230b6e718651efbf5e461dd576fa
Phan Thanh Thúy <90078502+tthuys@users.noreply.github.com>
2024-08-30 16:52:38 +0700

    reset

--- app/views/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-        <p>&copy; <?php echo date("Y"); ?> Group 02</p>
-    </footer>
-</body>
-</html>
--- public/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Location: /QLNS/public/index.php');
-exit();
-?>
